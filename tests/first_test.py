294b51551a746cc96499b4687be8f51dfc8adc62
Elhadji NGOM <ngomel.ehn@gmail.com>
2022-03-04 00:36:27 +0100

    updating

--- tests/first_test.py
+++ tests/first_test.py
@@ -4,3 +4,7 @@ from first_function import *
 def test_answer():
     """ test with assert """
     assert func(3) == 4, 'Do not behave as expected !!!'
+
+def test_fibonacci():
+    ''' test with assert '''
+    assert fibonacci(5) == 8, "Review your code"
